0077346923cb5c44de04164ccbd9d24efe91e4bf
tonghao.yuan <tonghao.yuan@csdental.com>
2021-07-20 12:31:12 +0800

    feat: add opengl debug

--- src/viewer/main_window.cpp
+++ src/viewer/main_window.cpp
@@ -1,4 +1,5 @@
 #include "viewer/main_window.h"
+#include <string>
 #include "common/color.h"
 #include "common/log.h"
 #include "viewer/icon.png.h"
@@ -33,6 +34,103 @@ void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action,
   }
 }
 
+void GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
+                            GLenum severity, GLsizei length, const GLchar* msg,
+                            const void* data) {
+  std::string _source;
+  std::string _type;
+  std::string _severity;
+
+  switch (source) {
+    case GL_DEBUG_SOURCE_API:
+      _source = "API";
+      break;
+
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+      _source = "WINDOW SYSTEM";
+      break;
+
+    case GL_DEBUG_SOURCE_SHADER_COMPILER:
+      _source = "SHADER COMPILER";
+      break;
+
+    case GL_DEBUG_SOURCE_THIRD_PARTY:
+      _source = "THIRD PARTY";
+      break;
+
+    case GL_DEBUG_SOURCE_APPLICATION:
+      _source = "APPLICATION";
+      break;
+
+    case GL_DEBUG_SOURCE_OTHER:
+      _source = "UNKNOWN";
+      break;
+
+    default:
+      _source = "UNKNOWN";
+      break;
+  }
+
+  switch (type) {
+    case GL_DEBUG_TYPE_ERROR:
+      _type = "ERROR";
+      break;
+
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+      _type = "DEPRECATED BEHAVIOR";
+      break;
+
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+      _type = "UDEFINED BEHAVIOR";
+      break;
+
+    case GL_DEBUG_TYPE_PORTABILITY:
+      _type = "PORTABILITY";
+      break;
+
+    case GL_DEBUG_TYPE_PERFORMANCE:
+      _type = "PERFORMANCE";
+      break;
+
+    case GL_DEBUG_TYPE_OTHER:
+      _type = "OTHER";
+      break;
+
+    case GL_DEBUG_TYPE_MARKER:
+      _type = "MARKER";
+      break;
+
+    default:
+      _type = "UNKNOWN";
+      break;
+  }
+
+  switch (severity) {
+    case GL_DEBUG_SEVERITY_HIGH:
+      _severity = "HIGH";
+      break;
+
+    case GL_DEBUG_SEVERITY_MEDIUM:
+      _severity = "MEDIUM";
+      break;
+
+    case GL_DEBUG_SEVERITY_LOW:
+      _severity = "LOW";
+      break;
+
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+      _severity = "NOTIFICATION";
+      break;
+
+    default:
+      _severity = "UNKNOWN";
+      break;
+  }
+
+  LOGD("{}: {} of {} severity, raised from {}: {}", id, _type, _severity,
+       _source, msg);
+}
+
 MainWindow::MainWindow() {
   glfwSetErrorCallback(glfw_error_callback);
   if (glfwInit() == 0) {
@@ -60,6 +158,26 @@ MainWindow::MainWindow() {
     throw std::runtime_error("glad load opengl failed");
   }
 
+  // see https://gist.github.com/liam-middlebrook/c52b069e4be2d87a6d2f for
+  // details
+  // =============== INIT DEBUG OUTPUT ================
+  // The following function calls should be made directly after OpenGL
+  // initialization.
+
+  // Enable the debugging layer of OpenGL
+  //
+  // GL_DEBUG_OUTPUT - Faster version but not useful for breakpoints
+  // GL_DEBUG_OUTPUT_SYNCHRONOUS - Callback is in sync with errors, so a
+  // breakpoint can be placed on the callback in order to get a stacktrace for
+  // the GL error.
+
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+  // Set the function that will be triggered by the callback, the second
+  // parameter is the data parameter of the callback, it can be useful for
+  // different contexts but isn't necessary for our simple use case.
+  glDebugMessageCallback(GLDebugMessageCallback, nullptr);
+
   //  set window icon
   {
     GLFWimage icon;
@@ -79,7 +197,6 @@ MainWindow::MainWindow() {
   ImGui_ImplOpenGL3_Init(kGLSLVersion);
 
   style::init_style(1.0F, 1.0F);
-  glEnable(GL_DEBUG_OUTPUT);
 }
 
 void MainWindow::Show() {
