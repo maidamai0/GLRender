7771ee6a00885292adb2990676cef3598997d8e3
maidamai0 <yuantonghao@gmail.com>
2021-08-04 21:40:03 +0800

    chore: make object biger

--- src/render/renderer.h
+++ src/render/renderer.h
@@ -28,7 +28,7 @@ class Renderder {
   GLint view_location_ = 0;
   GLint model_location_ = 0;
   GLint color_location_ = 0;
-  Camera camera_{{0.0F, 0.0F, 5.0F}};
+  Camera camera_{{0.0F, 0.0F, .5F}};
 
   glm::vec3 color_{0.8F, 0.4F, 0.2F};
 };
